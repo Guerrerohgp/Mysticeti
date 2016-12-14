3257047e21303eb8ba92b9633ea95556460b0277
Hugo A. Guerrero <guerrerohgp@gmail.com>
2016-12-14 01:24:56 -0400

    Adding composer, adding Dump, Enhace autocompile, enhace functions, simple style + included theter/jquery/boostrap

--- functions.php
+++ functions.php
@@ -1,145 +1,75 @@
 <?php
-/**
- * Mysticeti functions and definitions.
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package Mysticeti
- */
-
-if ( ! function_exists( 'mysticeti_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function mysticeti_setup() {
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on Mysticeti, use a find and replace
-	 * to change 'mysticeti' to the name of your theme in all the template files.
-	 */
-	load_theme_textdomain( 'mysticeti', get_template_directory() . '/languages' );
-
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
-
-	/*
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
-	add_theme_support( 'title-tag' );
-
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-	 */
-	add_theme_support( 'post-thumbnails' );
-
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => esc_html__( 'Primary', 'mysticeti' ),
-	) );
-
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
-	) );
-
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'mysticeti_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
-}
-endif;
-add_action( 'after_setup_theme', 'mysticeti_setup' );
 
 /**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
+ * Theme definition setup
  */
-function mysticeti_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'mysticeti_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'mysticeti_content_width', 0 );
-
+require get_template_directory() . '/app/boot.php';
 /**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ * Load Jetpack compatibility file.
  */
-function mysticeti_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'mysticeti' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'mysticeti' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'mysticeti_widgets_init' );
+require get_template_directory() . '/app/jetpack.php';
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/app/customizer.php';
+/**
+ * Custom functions that act independently of the theme templates.
+ */
+require get_template_directory() . '/app/extras.php';
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/app/custom-header.php';
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/app/template-tags.php';
+/**
+ * Theme Widgets definition, add here your wp widgets places
+ */
+require get_template_directory() . '/app/widgets.php';
+/**
+ * For the ajax calls
+ */
+require get_template_directory() . '/app/ajax.php';
+/**
+ * Where your functions may be
+ */
+require get_template_directory() . '/app/functions.php';
+
+
 
 /**
  * Enqueue scripts and styles.
  */
-function mysticeti_scripts() {
-	wp_enqueue_style( 'mysticeti-style', get_stylesheet_uri() );
+function mysticeti_scripts() {	
+	/**
+	 * Checks if file exist or was modified and compiles the scss to css if needed
+	 */
+	$compilefile = [//this file contains boostrap scss
+		'name'	=>	'app.scss',
+		'from'	=>	get_stylesheet_directory().'/css/scss/app.scss', 
+		'to'	=>	get_stylesheet_directory().'/css/app.css',
+	];
+	autoCompileScss($compilefile['name'],$compilefile['from'], $compilefile['to']);
 
+	/**
+	 * For CSS 
+	 */
+	wp_enqueue_style( 'mysticeti-style', get_stylesheet_directory_uri().'/css/mysticeti.css' );
+	wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri().'/css/bootstrap.min.css' );//boostrap css included
+	wp_enqueue_style( 'app', get_stylesheet_directory_uri().'/css/app.css' );//boostrap css included
+
+	/**
+	 * For JS
+	 */
 	wp_enqueue_script( 'mysticeti-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
 	wp_enqueue_script( 'mysticeti-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
+	wp_enqueue_script( 'jQuery', get_template_directory_uri() . '/js/jquery-3.1.1.min.js', array(), '20151215', true );
+	wp_enqueue_script( 'tether', get_template_directory_uri() . '/js/tether.min.js', array(), '20151215', true );
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '20151215', true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'mysticeti_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Custom functions that act independently of the theme templates.
- */
-require get_template_directory() . '/inc/extras.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-require get_template_directory() . '/inc/jetpack.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-require get_template_directory() . '/inc/boot/wp_mysticeti_boot.php';
--- inc/boot/wp_mysticeti_boot.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-require_once get_stylesheet_directory().'/inc/boot/scss/scsscompiler.php';
