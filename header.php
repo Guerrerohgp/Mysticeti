b1cdbbe587a724a4ba800e64cfdca071dd48a9c4
Hugo A. Guerrero <guerrerohgp@gmail.com>
2016-12-31 13:25:09 -0400

    update boostrap and header

--- header.php
+++ header.php
@@ -23,27 +23,23 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'mysticeti' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+	<header id="masthead" class="site-header" role="banner" style="border-bottom: 1px solid #e2e2e2;">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="navbar-brand site-title"><?php bloginfo( 'name' ); ?></a>
+                <!--<a class="" href="#">Project name</a>-->
+            </div>
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'mysticeti' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+            <div id="navbar" class="navbar-collapse collapse">
+	            <?php wp_nav_menu( [ 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'menu_class'=>'nav navbar-nav' ]); ?>
+            </div>
+        </div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
---
+++ layouts/page-load.php
@@ -0,0 +1,6 @@
+<?php
+	/**
+	 * Template Name: Load
+	 */
+
+?><h1><?=(microtime(true)-TTMMEE)?></h1>
