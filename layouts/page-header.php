573e9ce104abbeac387774af90192aa124f7fbf6
Allam Galán Rosa <allamgr@gmail.com>
2017-07-29 06:58:12 -0400

    services

--- layouts/page-header.php
+++ layouts/page-header.php
@@ -2,11 +2,88 @@
 	/**
 	 * Template Name: Header Title
 	 */
-?>
-get_header(); ?>
-    <div class="page-header-title">
-        the_title( '<h1 class="entry-title">', '</h1>' );
 
+get_header();
+?>
+    <style type="text/css">
+
+        #main-nav {
+            border-bottom: 1px solid rgba(255, 255, 255, 0.28);
+        }
+
+        #main-nav a{
+            color: #fff;
+        }
+
+        #main-nav .navbar-nav > .active > a, #main-nav .navbar-nav > .active > a:hover, #main-nav .navbar-nav > .active > a:focus{
+            color: #0A1921;
+        }
+
+        #main-nav a:hover, #main-nav a:focus{
+            opacity: 0.8;
+        }
+
+        #main-nav .navbar-collapse.collapse.in{
+            border-bottom: 1px solid <?php the_field('header_color') ?>;
+        }
+
+        .main-nav-back{
+            background: <?php the_field('header_color') ?>;
+        }
+
+        .page-template-page-header header > nav.navbar, .page-header-title{
+            background: <?php the_field('header_color') ?>;
+        }
+
+        .page-header-title {
+            position: relative;
+            padding: 50px 0 110px;
+        }
+
+        .page-header-title .entry-title{
+            font-weight: bold;
+        }
+
+        .page-template-page-header header > nav.navbar, .page-header-title .entry-title{
+            margin-top: 0;
+        }
+        .page-template-page-header header > nav.navbar, .page-header-title .entry-desc{
+            font-size: 1.2em;
+        }
+
+        header > nav.navbar .container .navbar-header .navbar-toggle{
+            border-color: #ddd;
+        }
+
+        header > nav.navbar .container .navbar-header .navbar-toggle:hover, header > nav.navbar .container .navbar-header .navbar-toggle:focus{
+            background-color: #ddd;
+        }
+
+        header > nav.navbar .container .navbar-header .navbar-toggle .icon-bar{
+            background-color: #fff;
+        }
+
+        .page-header-title .large-waves{
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 80px;
+            background: url(<?php echo get_template_directory_uri() ?>/images/page-header-waves-blue5.svg) top left repeat-x;
+            opacity: 0.3;
+        }
+    </style>
+
+    <div class="page-header-title text-center">
+        <div class="container">
+            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+            <div class="entry-desc">
+                <?php the_field('title_description'); ?>
+            </div>
+        </div>
+        <div class="large-waves">
+
+        </div>
     </div>
     <?php while ( have_posts() ) : the_post();
         get_template_part( 'template-parts/content', 'page-services' );
--- template-parts/content-page-services.php
+++ template-parts/content-page-services.php
@@ -11,3 +11,58 @@
 <div id="post-<?php the_ID(); ?>" class="entry-content">
     <?php //the_content(); ?>
 </div><!-- .entry-content -->
+
+<div id="services-boxes">
+
+    <div class="container text-center">
+
+        <div class="row">
+
+            <div class="service-box">
+                <div class="col-sm-6">
+                    <div class="service-box--inner">
+                        <img src="" alt="Custom Software Development">
+                        <h3 class="service-box--title">Custom Software Development</h3>
+                        <p class="service-box--desc">This is a service description number and we can provide to your
+                            company this service with hight quality. This is a service description
+                            number 1 and we can provide to your company this service with
+                            hight quality. </p>
+                        <ul class="list-unstyled service-box--skills">
+                            <li>Java</li>
+                            <li>C#</li>
+                            <li>PHP</li>
+                            <li>MySQL</li>
+                            <li>MSSQL</li>
+                            <li>MSSQL</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="service-box">
+                <div class="col-sm-6">
+                    <div class="service-box--inner">
+                        <img src="" alt="Custom Software Development">
+                        <h3 class="service-box--title">Web Development</h3>
+                        <p class="service-box--desc">This is a service description number and we can provide to your
+                            company this service with hight quality. This is a service description
+                            number 1 and we can provide to your company this service with
+                            hight quality. </p>
+                        <ul class="list-unstyled service-box--skills">
+                            <li>Javascript</li>
+                            <li>HTML</li>
+                            <li>CSS / SCSS</li>
+                            <li>Angular / Vue</li>
+                            <li>APIs</li>
+                            <li>Laravel</li>
+                            <li>Website</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+
+</div>
